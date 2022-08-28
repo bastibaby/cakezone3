d4b315c9a4cbbc18c26630198bda3090bf9c1af6
bastibaby <98473893+bastibaby@users.noreply.github.com>
2022-08-28 19:22:47 -0400

    Update form.php

--- form.php
+++ form.php
@@ -2,18 +2,13 @@
 
 
 
-
-$nombre=$_POST['nombre'];
-$email=$_POST['email'];
-$telefono=$_POST['telefono'];
-$asunto=$_POST['asunto'];
-$mensaje=$_POST['mensaje'];
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
 
 echo $correo. " ". $nombre. " ".$mensaje;
-
-echo"<script>alert ('Enviado correctamente')</script>";
-echo"<script> setTimeout (\"location.href='index.html'\",1000)</script>";
-    
 
 
 
